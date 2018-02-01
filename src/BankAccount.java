e3829e5cf8db097a38a05602f9f26d4d0e79e642
kgullanat <30823949+kgullanat@users.noreply.github.com>
2018-02-01 09:25:15 +0700

    11

---
+++ src/BankAccount.java
@@ -0,0 +1,29 @@
+
+public class BankAccount {
+
+	private int balance;
+	
+	public BankAccount(int money) {
+		balance = money;
+	}
+	public BankAccount() {
+		balance = 0;
+	}
+	
+	public void deposit(int money) {
+		balance += money;
+	
+		
+	}
+
+	public int getBalance() {
+		
+		return balance;
+	}
+
+	public void withdraw(int money) {
+		balance -= money;
+		
+	}
+
+}
---
+++ src/BankAccountTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import org.junit.Test;
+
+
+
+public class BankAccountTest {
+
+	@Test
+	public void TestDeposit(){
+		BankAccount acc = new BankAccount();
+		acc.deposit(50);
+		assertEquals(acc.getBalance(),50);
+	}
+
+	@Test
+	public void TestWithDraw(){
+		BankAccount acc = new BankAccount(75);
+		acc.withdraw(50);
+		assertEquals(acc.getBalance(),25);
+
+}
+}
